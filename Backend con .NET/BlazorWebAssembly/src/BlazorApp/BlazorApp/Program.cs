7e06a73ef524785464b0d4ae3318719c736bc7da
sermedev <sermedev@outlook.com>
2024-03-15 06:38:29 +0100

    build(BlazorWebAssembly): Add Platzi API Url

--- Backend con .NET/BlazorWebAssembly/src/BlazorApp/BlazorApp/Program.cs
+++ Backend con .NET/BlazorWebAssembly/src/BlazorApp/BlazorApp/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var apiUrl = builder.Configuration.GetValue<string>("apiPlatziUrl");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 
 await builder.Build().RunAsync();
