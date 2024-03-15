fce112d84e056f6a8c7c6ba3572059ae64d597b3
sermedev <sermedev@outlook.com>
2024-03-15 07:00:56 +0100

    build(BlazorWebAssembly): Add Product model

---
+++ Backend con .NET/BlazorWebAssembly/src/BlazorApp/BlazorApp/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int Category { get; set; }
+        public List<string> Images { get; set; }
+    }
+}
