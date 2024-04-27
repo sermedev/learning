b6ac813d997eb44f047a2d69217567354db937fe
sermedev <sermedev@outlook.com>
2024-04-27 08:32:25 +0200

    doc: Add primitivos section

---
+++ DesarrolloWeb/JavaScript/javascript-moderno/src/01_Fundamentos/assets/js/03_primitivos.js
@@ -0,0 +1,58 @@
+// STRING
+//________________________________________________
+
+let nombre = 'Michael Jordan';
+console.log(nombre);
+
+nombre = 'Ben';
+console.log(nombre);
+
+nombre = "Pato Lucas";
+nombre = `Lucas`;
+console.log(nombre);
+
+console.log(typeof nombre);
+
+nombre = 123;
+console.log(typeof nombre);
+
+
+// BOOLEAN
+//________________________________________________
+
+let esMarvel = true;
+console.log(typeof esMarvel);
+
+
+// NUMBER
+//________________________________________________
+
+let edad = 40;
+console.log(typeof edad);
+
+edad = 33.001
+console.log(typeof edad);
+
+
+// UNDEFINED
+//________________________________________________
+
+let superPoderDeSpiderMan ;
+console.log(typeof superPoderDeSpiderMan);
+
+
+// NULL
+//________________________________________________
+let soyNull = null;
+console.log(soyNull);
+
+
+// SYMBOL
+//________________________________________________
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+console.log(typeof symbol1);
+console.log(typeof symbol2);
+
+console.log(symbol1 === symbol2);
