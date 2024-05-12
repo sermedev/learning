863cd805cfbf7ae43d4a7c658ed509525c9b95c9
sermedev <sermedev@outlook.com>
2024-05-12 08:32:27 +0200

    feat(js-moderno): Add Patron Modulo

--- desarrollo-web/javascript/js-moderno/src/05_lab-blackjack/assets/js/game.js
+++ desarrollo-web/javascript/js-moderno/src/05_lab-blackjack/assets/js/game.js
@@ -1,174 +1,168 @@
-// //#region Info
-
-/**
- * 2C = Two of Clubs (Tréboles)
- * 2D = Two of Diamons (Diamantes)
- * 2H = Two of Hearts (Corazones)
- * 2S = Two of Spades (Picas)
- */
-
-//#endregion
+(() => {
+    'use strict'
 
 
-//#region Properties
+    //#region Properties
 
-const cardTypes = ['C', 'D', 'H', 'S'];
-const specialCards = ['A', 'J', 'Q', 'K'];
+    const cardTypes = ['C', 'D', 'H', 'S'];
+    const specialCards = ['A', 'J', 'Q', 'K'];
 
-let deck = [];
-let playerPoints = 0;
-let pcPoints = 0;
+    let deck = [];
+    let playerPoints = 0;
+    let pcPoints = 0;
 
-//#endregion
-
-
-//#region HTML-Refs
-
-const btnAsk     = document.querySelector('#btnAsk');
-const btnNewGame = document.querySelector('#btnNewGame');
-const btnStop = document.querySelector('#btnStop');
-
-const playerCounterPoints = document.querySelector('#player-counter-points');
-const playerDivCards      = document.querySelector('#player-cards');
-
-const pcCounterPoints = document.querySelector('#pc-counter-points');
-const pcDivCards      = document.querySelector('#pc-cards');
-
-//#endregion
+    //#endregion
 
 
-//#region Functions
+    //#region HTML-Refs
 
-const createDeck = () => {
+    const btnAsk     = document.querySelector('#btnAsk');
+    const btnNewGame = document.querySelector('#btnNewGame');
+    const btnStop = document.querySelector('#btnStop');
 
-    for (let i = 2; i <= 10; i++) {
+    const playerCounterPoints = document.querySelector('#player-counter-points');
+    const playerDivCards      = document.querySelector('#player-cards');
+
+    const pcCounterPoints = document.querySelector('#pc-counter-points');
+    const pcDivCards      = document.querySelector('#pc-cards');
+
+    //#endregion
+
+
+    //#region Functions
+
+    const createDeck = () => {
+
+        for (let i = 2; i <= 10; i++) {
+            for (let type of cardTypes) {
+                deck.push(i + type);
+            }
+        }
+
         for (let type of cardTypes) {
-            deck.push(i + type);
+            for (let special of specialCards) {
+                deck.push(special + type);
+            }
         }
+
+        deck = _.shuffle(deck);
+
+        return deck;
     }
 
-    for (let type of cardTypes) {
-        for (let special of specialCards) {
-            deck.push(special + type);
+    const askCard = () => {
+
+        if (deck.length === 0) {
+            throw 'No more cards in deck';
         }
+
+        const card = deck.pop();
+
+        return card;
     }
 
-    deck = _.shuffle(deck);
+    const getCardValue = (card) => {
 
-    return deck;
-}
-
-const askCard = () => {
-
-    if (deck.length === 0) {
-        throw 'No more cards in deck';
+        const value = card.substring(0, card.length - 1);
+        return  ( isNaN(value) ) ?
+                ( value === 'A') ? 11 : 10
+                : value * 1;
     }
 
-    const card = deck.pop();
+    const playPc = ( minimunPoints ) => {
+        
+        do{
+            const card = askCard();
+            pcPoints += getCardValue(card);
+            pcCounterPoints.textContent = pcPoints;
 
-    return card;
-}
+            const cardImg = document.createElement('img');
+            cardImg.classList.add('cards');
+            cardImg.src = `assets/cards/${ card }.png`;
+            pcDivCards.append(cardImg);
 
-const getCardValue = (card) => {
+            if (minimunPoints > 21){
+                break;
+            }
 
-    const value = card.substring(0, card.length - 1);
-    return  ( isNaN(value) ) ?
-            ( value === 'A') ? 11 : 10
-            : value * 1;
-}
+        }while ((minimunPoints < 21) && (pcPoints < minimunPoints)); 
+            
 
-const playPc = ( minimunPoints ) => {
-    
-    do{
+        setTimeout(() => {
+            
+            if(pcPoints === minimunPoints ){
+                alert('Empate 🥲');
+            }else if (minimunPoints > 21){
+                alert('Perdiste ☹️');
+            }else if(pcPoints > 21 || minimunPoints > pcPoints){
+                alert('Ganaste 😀');
+            }else{
+                alert('Perdiste ☹️');
+            };
+        }, 300);
+    };
+
+    //#endregion
+
+
+    //#region Events
+
+    btnAsk.addEventListener('click', () => {
+
+        btnStop.disabled = false;
+
         const card = askCard();
-        pcPoints += getCardValue(card);
-        pcCounterPoints.textContent = pcPoints;
+        playerPoints += getCardValue( card);
+        playerCounterPoints.textContent = playerPoints;
 
         const cardImg = document.createElement('img');
         cardImg.classList.add('cards');
         cardImg.src = `assets/cards/${ card }.png`;
-        pcDivCards.append(cardImg);
+        playerDivCards.append(cardImg);
 
-        if (minimunPoints > 21){
-            break;
+        if(playerPoints > 21){
+            console.log('Lo siento mucho, perdiste');
+            btnAsk.disabled = true;
+            btnStop.disabled = true;
+            playPc(playerPoints);
+        }else if(playerPoints === 21){
+            console.log('21, ganaste!!!');
+            btnAsk.disabled = true;
+            btnStop.disabled = true;
+            playPc(playerPoints);
         }
-
-    }while ((minimunPoints < 21) && (pcPoints < minimunPoints)); 
         
+    });
 
-    setTimeout(() => {
-        
-        if(pcPoints === minimunPoints ){
-            alert('Empate 🥲');
-        }else if (minimunPoints > 21){
-            alert('Perdiste ☹️');
-        }else if(pcPoints > 21 || minimunPoints > pcPoints){
-            alert('Ganaste 😀');
-        }else{
-            alert('Perdiste ☹️');
-        };
-    }, 300);
-};
-
-//#endregion
-
-
-//#region Events
-
-btnAsk.addEventListener('click', () => {
-
-    btnStop.disabled = false;
-
-    const card = askCard();
-    playerPoints += getCardValue( card);
-    playerCounterPoints.textContent = playerPoints;
-
-    const cardImg = document.createElement('img');
-    cardImg.classList.add('cards');
-    cardImg.src = `assets/cards/${ card }.png`;
-    playerDivCards.append(cardImg);
-
-    if(playerPoints > 21){
-        console.log('Lo siento mucho, perdiste');
+    btnStop.addEventListener('click', () => {
         btnAsk.disabled = true;
         btnStop.disabled = true;
         playPc(playerPoints);
-    }else if(playerPoints === 21){
-        console.log('21, ganaste!!!');
-        btnAsk.disabled = true;
-        btnStop.disabled = true;
-        playPc(playerPoints);
-    }
-    
-});
+    });
 
-btnStop.addEventListener('click', () => {
+
+    btnNewGame.addEventListener('click', () => {
+        createDeck();
+
+        playerPoints = 0;
+        playerCounterPoints.textContent = playerPoints;
+        
+        pcPoints = 0;
+        pcCounterPoints.textContent = pcPoints;
+
+        btnAsk.disabled = false;
+
+        playerDivCards.innerHTML = '';
+        pcDivCards.innerHTML = '';
+    });
+
+
+    //#endregion
+
+
+    // Initialization when page loading
     btnAsk.disabled = true;
     btnStop.disabled = true;
-    playPc(playerPoints);
-});
 
 
-btnNewGame.addEventListener('click', () => {
-    createDeck();
-
-    playerPoints = 0;
-    playerCounterPoints.textContent = playerPoints;
-    
-    pcPoints = 0;
-    pcCounterPoints.textContent = pcPoints;
-
-    btnAsk.disabled = false;
-
-    playerDivCards.innerHTML = '';
-    pcDivCards.innerHTML = '';
-});
-
-
-//#endregion
-
-
-
-// Initialization when page loading
-btnAsk.disabled = true;
-btnStop.disabled = true;
+})();
