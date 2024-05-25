ba34b8bd5167d250205b949f9e674c3c827abbcb
sermedev <sermedev@outlook.com>
2024-05-25 07:25:50 +0200

    feat(design-patterns) Add Singleton Log in Asp.Net

---
+++ basics/design-patterns-csharp/src/DesignPatterns/DesignPatternsAsp/Configurations/MyConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace DesignPatternsAsp.Configurations
+{
+    public class MyConfig
+    {
+        public string PathLog { get; set; }
+    }
+}
---
+++ basics/design-patterns-csharp/src/DesignPatterns/DesignPatternsAsp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using DesignPatternsAsp.Configurations;
+using DesignPatternsAsp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Diagnostics;
+using Tools;
+
+namespace DesignPatternsAsp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IOptions<MyConfig> _config;
+
+        public HomeController(IOptions<MyConfig> config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            Log.GetInstance(_config.Value.PathLog).Save("Here in index");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            Log.GetInstance(_config.Value.PathLog).Save("Here in Privacy");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
